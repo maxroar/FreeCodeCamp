8ec6454fda9b67e67703a6863651ee228a6771aa
Max Rohrer <mxrohrer@gmail.com>
2016-12-26 11:48:58 -0800

    set up geolocation call for on page load

---
+++ weatherApp/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+  //function to convert fahrenheit to celcius
+  function fToC(f){
+    var cel= (f-32)*5/9;
+    $('h3').html(`${cel}`)
+  }
+  //function to conver celcius to fahrenheit
+  function cToF(c){
+    var fah = (9/5)*c + 32;
+    return fah;
+  }
+  //variables to hold the user's lat and longitude
+  var lat = 0;
+  var lon = 0;
+  //variables to hold city name and temp
+  var cityName;
+  var temp;
+  //a function to set the location of the user based on latitude and longitutd on pageload
+  function setLoc(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(setPos);
+      //set the lat and lon variables to integers
+    }else alert('Geolocation has been disabled by your browser. Please use the search bar.');
+    function setPos(position){
+      lat = Math.floor(position.coords.latitude);
+      lon = Math.floor(position.coords.longitude);
+      console.log(lat, lon);
+      callCoords();
+    }
+  }
+  function callCoords(){
+    $.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=be0f61e7a55bf452fa1de4d1ba944b32`, function(response){
+      console.log(response);
+      setVars(response);
+    });
+  }
+  setLoc();
+
+  function setVars(response){
+    cityName = response.name;
+    temp = response.main.temp;
+    displayContent();
+  }
+  //a function to change the formatting based on the temp
+  function displayContent(){
+    if(temp < 50){
+      $('#style').html(`<link id="style" rel="stylesheet" href="styles/cold.css">`);
+    }else if(temp <85){
+      $('#style').html(`<link id="style" rel="stylesheet" href="styles/perfect.css">`);
+    }else if(temp >=85){
+      $('#style').html(`<link id="style" rel="stylesheet" href="styles/hot.css">`);
+    }else $('#style').html(`<link id="style" rel="stylesheet" href="styles/main.css">`);
+
+    $('#resultDisplay').html(`
+      <h2>${cityName}</h2>
+      <h3>${temp}&#8457;</h3>
+      <button id="f" type="button">Fahrenheit</button>
+      <button id="c" type="button">Celcius</button>
+      `);
+  }
+  //a function to set the
+  $('form').on('submit', function(){
+    var cityName = $(this).serialize();
+    console.log(cityName);
+    $.get(`http://api.openweathermap.org/data/2.5/weather?${cityName}&units=imperial&appid=be0f61e7a55bf452fa1de4d1ba944b32`, function(response){
+      setVars(response);
+      displayContent();
+    });
+    return false;
+  });
+});
